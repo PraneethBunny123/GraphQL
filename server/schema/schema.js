0b7ec8193fef815921664ef9c87ae1f72c53cb0b
praneeth <gp.gunturu@gmail.com>
2025-07-23 13:49:33 -0500

    exporting the new schema. the starting query would be rootQuery

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql')
 
-const {GraphQLObjectType, GraphQLstring} = graphql;
+const {GraphQLObjectType, GraphQLstring, GraphQLSchema} = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -11,20 +11,27 @@ const BookType = new GraphQLObjectType({
     })
 })
 
-const AuthorType = new GraphQLObjectType({
-    name: 'Author',
-    fields: () => ({
-        name: {type: GraphQLstring},
-        age: {type: GraphQLstring}
-    })
-})
+// const AuthorType = new GraphQLObjectType({
+//     name: 'Author',
+//     fields: () => ({
+//         name: {type: GraphQLstring},
+//         age: {type: GraphQLstring}
+//     })
+// })
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         book: {
             type: BookType,
-            args: {type: GraphQLstring}
+            args: {type: GraphQLstring},
+            resolve(parent, args) {
+                // code to get data from DB 
+            }
         }
     }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
