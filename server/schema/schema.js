c9502ea2d3e21685ed891ada9cfb8579fdf85af5
praneeth <gp.gunturu@gmail.com>
2025-07-25 13:34:36 -0500

    replcing the dummy data with the models from the mongoose

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,9 @@
 const graphql = require('graphql')
 
+// getting the models 
+const Book = require('../models/book');
+const Author = require('../models/author');
+
 const {
     GraphQLObjectType, 
     GraphQLString, 
@@ -9,24 +13,28 @@ const {
     GraphQLList
 } = graphql;
 
+///////////////////////////////////////////////////////////
+
 // Dummy Data
 
-var books = [
-    {name: 'Maid Sama', genre: 'romcom', id: '1', authorId: '1'},
-    {name: 'Demon Slayer', genre: 'action', id: '2', authorId: '2'},
-    {name: 'HxH', genre: 'Adventure', id: '3', authorId: '3'},
-    {name: 'One Piece', genre: 'Peak', id: '4', authorId: '1'},
-    {name: 'JJK', genre: 'Sorcery', id: '5', authorId: '3'},
-    {name: 'Haikyuu', genre: 'Sports', id: '6', authorId: '1'},
-    {name: 'Blue Lock', genre: 'Ego', id: '7', authorId: '3'},
-    {name: 'Hells Paradise', genre: 'Horror', id: '8', authorId: '2'},
-]
+// var books = [
+//     {name: 'Maid Sama', genre: 'romcom', id: '1', authorId: '1'},
+//     {name: 'Demon Slayer', genre: 'action', id: '2', authorId: '2'},
+//     {name: 'HxH', genre: 'Adventure', id: '3', authorId: '3'},
+//     {name: 'One Piece', genre: 'Peak', id: '4', authorId: '1'},
+//     {name: 'JJK', genre: 'Sorcery', id: '5', authorId: '3'},
+//     {name: 'Haikyuu', genre: 'Sports', id: '6', authorId: '1'},
+//     {name: 'Blue Lock', genre: 'Ego', id: '7', authorId: '3'},
+//     {name: 'Hells Paradise', genre: 'Horror', id: '8', authorId: '2'},
+// ]
 
-var authors = [
-    {name: 'Luffy', age: 42, id: '1'},
-    {name: 'Tanjiro', age: 27, id: '2'},
-    {name: 'Gon', age: 33, id: '3'}
-]
+// var authors = [
+//     {name: 'Luffy', age: 42, id: '1'},
+//     {name: 'Tanjiro', age: 27, id: '2'},
+//     {name: 'Gon', age: 33, id: '3'}
+// ]
+
+////////////////////////////////////////////////////////////
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -37,7 +45,7 @@ const BookType = new GraphQLObjectType({
         author: {
             type: AuthorType,
             resolve(parent, args) {
-                return authors.find(author => author.id == parent.authorId )
+                // return authors.find(author => author.id == parent.authorId )
             }
         }
     })
@@ -52,7 +60,7 @@ const AuthorType = new GraphQLObjectType({
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                return books.filter(book => book.authorId === parent.id)
+                // return books.filter(book => book.authorId === parent.id)
             }
         }
     })
@@ -66,26 +74,26 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
                 // code to get data from DB 
-                return books.find(book => book.id == args.id)
+                // return books.find(book => book.id == args.id)
             }
         },
         author: {
             type: AuthorType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
-                return authors.find(author => author.id === args.id)
+                // return authors.find(author => author.id === args.id)
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                return books
+                // return books
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args) {
-                return authors
+                // return authors
             }
         }
     }
