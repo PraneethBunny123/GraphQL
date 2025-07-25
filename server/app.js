a3eb7fa75ef946a7116e62665d7754b55c9da06e
praneeth <gp.gunturu@gmail.com>
2025-07-25 12:29:48 -0500

    added .env and added the mongoose cluster url to it. installed mogoose in the server and connected to the server. Logging a message when connecting to the database

--- server/app.js
+++ server/app.js
@@ -2,7 +2,22 @@ const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
 
+require('dotenv').config()
+const mongoose = require('mongoose')
+
 const app = express()
+
+// connect to mongodb atlas
+// mongoose.connect('mongodb+srv://bunny:bunny@cluster0.xp1gj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+// mongoose.connection.once('open', () => {
+//     console.log('connected to database')
+// })
+
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', () => {
+    console.log('database connection')
+})
+
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
