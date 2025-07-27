366761bdd6c41278dcab828308195b796ea963b2
praneeth <gp.gunturu@gmail.com>
2025-07-26 23:55:12 -0500

    removed the text

--- server/app.js
+++ server/app.js
@@ -7,12 +7,6 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-// connect to mongodb atlas
-// mongoose.connect('mongodb+srv://bunny:bunny@cluster0.xp1gj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-// mongoose.connection.once('open', () => {
-//     console.log('connected to database')
-// })
-
 mongoose.connect(process.env.MONGO_URI)
 mongoose.connection.once('open', () => {
     console.log('database connection')
