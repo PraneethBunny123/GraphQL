c4c757ce52070eb72aa5e964d94159aa815c511b
praneeth <gp.gunturu@gmail.com>
2025-07-27 00:40:36 -0500

    Installed apollo client and imported the ApolloClient

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
+import BookList from "./components/BookList.jsx"
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
+// const client = new ApolloClient({
+//     uri: "http://localhost:4000/graphql"
+// })
 
 function App() {
 
-  return (
-    <div>Hello</div>
-  )
+    return (
+        <div id="main">
+            <h1>Ninja</h1>
+            <BookList />
+        </div>
+    )
 }
 
 export default App
