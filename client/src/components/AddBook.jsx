40dcd4673bd65cb138e3b136125fc80bf2a95f57
praneeth <gp.gunturu@gmail.com>
2025-07-28 17:52:24 -0500

    few changes

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -38,50 +38,50 @@ export default function AddBook() {
 
     return (
         <>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Book Name:</label><br />
-                <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleOnChange}
-                    required
-                />
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Book Name:</label><br />
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleOnChange}
+                        required
+                    />
+                </div>
 
-            <div>
-                <label>Genre:</label><br />
-                <input
-                    type="text"
-                    name="genre"
-                    value={formData.genre}
-                    onChange={handleOnChange}
-                    required
-                />
-            </div>
+                <div>
+                    <label>Genre:</label><br />
+                    <input
+                        type="text"
+                        name="genre"
+                        value={formData.genre}
+                        onChange={handleOnChange}
+                        required
+                    />
+                </div>
 
-            <div>
-                <label>Author:</label><br />
-                <select 
-                    name="authorId"
-                    value={formData.authorId}
-                    onChange={handleOnChange}  
-                    required  
-                >
-                    <option value="">-- Select Author --</option>
-                    {loading && <option>Loading Authors...</option>}
-                    {!loading && data.authors.map(author => (
-                        <option key={author.id} value={author.id}>{author.name}</option>
-                    ))}
-                </select>
-            </div>
+                <div>
+                    <label>Author:</label><br />
+                    <select 
+                        name="authorId"
+                        value={formData.authorId}
+                        onChange={handleOnChange}  
+                        required  
+                    >
+                        <option value="">-- Select Author --</option>
+                        {loading && <option>Loading Authors...</option>}
+                        {!loading && data?.authors?.map(author => (
+                            <option key={author.id} value={author.id}>{author.name}</option>
+                        ))}
+                    </select>
+                </div>
 
-            <button type="submit">Add Book</button>
-        </form>
-        {addBookLoading && <p>Adding book...</p>}
-        {addBookError && <p style={{color: 'red'}}>Error: {addBookError.message}</p>}
-        {addBookData && <p style={{color: 'green'}}>Book added: {addBookData.addBook.name} with id: {addBookData.addBook.id}  </p>}
+                <button type="submit" disabled={addBookLoading}>{addBookLoading ? "Adding..." : "Add Book"}</button>
+            </form>
+            {addBookLoading && <p>Adding book...</p>}
+            {addBookError && <p style={{color: 'red'}}>Error: {addBookError.message}</p>}
+            {addBookData && <p style={{color: 'green'}}>Book added: {addBookData.addBook.name} with id: {addBookData.addBook.id}  </p>}
         </>
     )
 }
