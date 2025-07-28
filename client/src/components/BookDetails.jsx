2054bf04aa7796fe12353be496937cbd7922a169
praneeth <gp.gunturu@gmail.com>
2025-07-28 18:26:46 -0500

    created a BookDetails component need to pass the actual id as a variable

--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@apollo/client"
 import { getSingleBookQuery } from "../queries/queries"
 
-export default function BookDetails() {
-    const {data, loading, error} = useQuery(getSingleBookQuery)
+export default function BookDetails({id}) {
+    const {data, loading, error} = useQuery(getSingleBookQuery, {variables: {id: id}})
+    console.log(data)
 
     return (
         <div>
--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@apollo/client"
 import { getBooksQuery } from "../queries/queries"
+import BookDetails from "./BookDetails"
 
 export default function BookList() {
     const {loading, error, data} = useQuery(getBooksQuery)
@@ -19,6 +20,7 @@ export default function BookList() {
                     <li key={book.id}>{book.name}</li>
                 ))}
             </ul>
+            <BookDetails />
         </div>
     )
 }
