c58e826f56070b99797a637ca97a06e231f82182
praneeth <gp.gunturu@gmail.com>
2025-07-29 17:57:01 -0500

    Styled the BookDetails

--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -1,29 +1,29 @@
-import { useQuery } from "@apollo/client"
-import { getSingleBookQuery } from "../queries/queries"
+import { useQuery } from "@apollo/client";
+import { getSingleBookQuery } from "../queries/queries";
 
-export default function BookDetails({id}) {
-    const {data, loading, error} = useQuery(getSingleBookQuery, {variables: {id: id}})
-    
+export default function BookDetails({ id }) {
+    const { data, loading, error } = useQuery(getSingleBookQuery, { variables: { id } });
 
-    if(loading) {
-        return <p>Loading Book details...</p>
+    if (loading) {
+        return <p className="text-gray-600">Loading book details...</p>;
     }
-    
-    if(error) {
-        return <p>Failed to get the book details.</p>
+
+    if (error) {
+        return <p className="text-red-600">Failed to get the book details.</p>;
     }
 
     return (
-        <div>
-            <h3>{data.book.name}</h3>
-            <p>{data.book.genre}</p>
-            <p>{data.book.author.name}</p>
-            <p>All books by this author:</p>
-            <ul>
-                {data.book.author.books.map(book => (
+        <div className="bg-white rounded-lg shadow-md p-6 text-gray-800">
+            <h3 className="text-2xl font-semibold text-accent mb-2">{data.book.name}</h3>
+            <p className="mb-1"><span className="font-medium">Genre:</span> {data.book.genre}</p>
+            <p className="mb-4"><span className="font-medium">Author:</span> {data.book.author.name}</p>
+
+            <p className="font-medium mb-2">Other books by this author:</p>
+            <ul className="list-disc list-inside space-y-1">
+                {data.book.author.books.map((book) => (
                     <li key={book.id}>{book.name}</li>
                 ))}
             </ul>
         </div>
-    )
+    );
 }
